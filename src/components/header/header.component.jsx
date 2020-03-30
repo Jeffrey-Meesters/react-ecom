f739f67848ddb81baeedb9e2c600e7fc5841cc40
Jeffrey Meesters <jmeesters@harlemnext.com>
2020-03-30 22:05:27 +0200

    Add signin to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,6 +16,9 @@ const Header = () => (
       <Link className="option" to="/contact">
         Contact
       </Link>
+      <Link className="option" to="/signin">
+        Sign in
+      </Link>
     </div>
   </div>
 );
