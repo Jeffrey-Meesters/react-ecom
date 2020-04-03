a8cbc1f9034cefb8303ffd4ad0c22798733ac3e2
Jeffrey Meesters <jmeesters@harlemnext.com>
2020-04-03 18:26:48 +0200

    add cart utils to goupr items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={
-      isGoogleSignIn ? "google-sign-in custom-button" : "custom-button"
-    }
+    className={`
+    ${isGoogleSignIn ? "google-sign-in custom-button" : "custom-button"}
+      ${inverted ? "inverted" : ""}
+    `}
     {...otherProps}
   >
     {children}
